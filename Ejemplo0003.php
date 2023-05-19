0a0fc0affe44e1be0c8899a1a11d427a5ada5b5a
Sfletes <133998800+Sfletes@users.noreply.github.com>
2023-05-19 14:14:02 -0500

    Primer commit

---
+++ Ejemplo0003.php
@@ -0,0 +1,23 @@
+<!-- Manual de PHP --> 
+<html> 
+<head> 
+ <title>Ejemplo de PHP</title> 
+</head> 
+<body> 
+<?php 
+ $a = 8; 
+ $b = 3; 
+ echo $a, "<br>";
+ echo $b, "<br>";
+"<br/>";
+ echo $a + $b,"<br>"; 
+ echo $a - $b,"<br>"; 
+ echo $a * $b,"<br>"; 
+ echo $a / $b,"<br>"; 
+ $a++; 
+ echo $a,"<br>"; 
+ $b--; 
+ echo $b,"<br>"; 
+?> 
+</body> 
+</html> 
